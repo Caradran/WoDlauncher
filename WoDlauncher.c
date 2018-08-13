2f05a9f3b4fe9e71cdece2f22d4f2da0f488967a
eliott suits <esuits@rochegude.saclay.inria.fr>
2018-08-13 19:17:41 +0200

    bordel c'etait plus dur que ce que je pensais

--- WoDlauncher.c
+++ WoDlauncher.c
@@ -23,6 +23,7 @@ t_des	WoD_result(int n, int seuil, int again, int rote, int flag)
 {
 	int		i = 0;
 	int		d = 0;
+	int		k = 0;
 	t_des	des = {0, 0, 0};
 	t_des	relance = {0, 0, 0};
 
@@ -31,36 +32,37 @@ t_des	WoD_result(int n, int seuil, int again, int rote, int flag)
 	while (i < n)
 	{
 		d = D_10();
-		printf("%d\n", d);
+		if (!flag)
+			printf("rote : %d\n", d);
+		else
+			printf("%d\n", d);
 		if (d >= seuil)
 		{
+			k++;
 			des.reussites++;
 			if (d >= again)
 				des.relances++;
 		}
 		if (d == 1)
 		{
+			des.relances--;
 			des.reussites--;
 			des.uns++;
 		}
 		i++;
 	}
-	if (rote > 0)
+	if (rote > 0 && n - k > 0)
 	{
-		if (des.reussites - des.uns >= 0)
-			relance = WoD_result(n - des.reussites + des.uns, seuil, again, rote - 1, 0);
-		else
-		{
-			relance = WoD_result(n, seuil, again, rote - 1, 0);
-			des.reussites = 0;
-		}
+		relance = WoD_result(n - k, seuil, again, rote - 1, 0);
 		des.relances = des.relances + des.uns - relance.uns + relance.relances;
 		des.reussites += relance.reussites;
+		des.uns = relance.uns;
 	}
 	if (des.relances > 0 && flag)
 	{
-		relance = WoD_result(des.relances - des.uns, seuil, again, 0, 1);
-		des.reussites += relance.reussites + relance.uns;
+		relance = WoD_result(des.relances, seuil, again, 0, 1);
+		if (relance.reussites > 0)
+			des.reussites += relance.reussites;
 	}
 	return (des);
 }
@@ -80,7 +82,7 @@ int main()
 		i = 0;
 //		while (i < 100000)
 //		{
-			printf("reussites : %d\n", WoD_result(2, 7, 10, 1, 1).reussites);
+			printf("reussites : %d\n", WoD_result(2, 7, 7, 1, 1).reussites);
 //			i++;
 //		}
 
